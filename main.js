eee576fce5a9357a54249228e1d142b8b914ac8a
Jason16803 <jason@thewebsmithagency.com>
2025-06-27 23:56:06 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+//For the tabs function
+function openMode(evt, modeName) {
+  
+  var i, tabcontent, tablinks, tabsubcontent, tabsublinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(modeName).style.display = "grid";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
+
+
+
+
+//For the back to top button
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function toggleMobileMenu(menu){
+  menu.classList.toggle('open');
+}
